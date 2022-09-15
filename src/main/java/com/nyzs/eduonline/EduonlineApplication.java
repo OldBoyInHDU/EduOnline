d4240a9a6c4f6021f089176712491c134320eaa9
OldBoyInHDU <973157930@qq.com>
2022-09-15 13:48:08 +0800

    update .gitignore

--- src/main/java/com/nyzs/eduonline/EduonlineApplication.java
+++ src/main/java/com/nyzs/eduonline/EduonlineApplication.java
@@ -2,12 +2,18 @@ package com.nyzs.eduonline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EduonlineApplication {
+public class EduonlineApplication /*extends SpringBootServletInitializer*/ {
 
     public static void main(String[] args) {
         SpringApplication.run(EduonlineApplication.class, args);
     }
 
+    /*@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EduonlineApplication.class);
+    }*/
 }
